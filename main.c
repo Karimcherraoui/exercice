116a087f2cf26d3cab5536527d705d027f54487f
karimcherraoui <cherraoui.karim96@gmail.com>
2022-09-07 10:02:04 +0100

    creation du code pour le premier exercice

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int age ;
+    int date_naissance = 1996 ;
+    char* nom = "karim cherraoui" ;
+    float temperature = 22.3 ;
+    age = 2022 - date_naissance ;
+    printf("Je m'appelle %s j'ai %d ans et la temperature d'aujourd'hui est %.2f degree ",nom ,age ,temperature);
+
+
+    return 0;
+}
